297b23bd2afbc2f911f7d0a5313ff91176cf4f49
FUXOZ <151724735+FUXOZ@users.noreply.github.com>
2024-06-05 15:22:47 +0700

    push

--- PA2-kel04/app/Http/Controllers/Admin/DashboardController.php
+++ PA2-kel04/app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Order;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        // Menghitung total penjualan
+        $total_penjualan = Order::select(DB::raw("CAST(SUM(grand_total) as int) as total_penjualan"))
+            ->groupBy(DB::raw("DAY(created_at)"))
+            ->pluck('total_penjualan');
+
+        // Menghitung hari
+        $hari = Order::select(DB::raw("DAYNAME(created_at) as hari"))
+            ->groupBy(DB::raw("DAYNAME(created_at)"))
+            ->pluck('hari');
+
+        return view('admin.dashboard', compact('total_penjualan', 'hari'));
     }
 }
--- PA2-kel04/resources/views/admin/dashboard.blade.php
+++ PA2-kel04/resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
                 <div class="col-sm-6">
                     <h1 class="m-0">{{ __('Dashboard') }}</h1>
                 </div><!-- /.col -->
-            </div><!-- /.row -->
+            </div><!-- /.row -->    
         </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
@@ -28,6 +28,46 @@
                 </div>
             </div>
             <!-- /.row -->
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">Grafik Penjualan </div>
+                            <div class="card-body">
+                                <div id="grafik"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <script src="https://code.highcharts.com/highcharts.js"></script>
+            <script type="text/javascript">
+                var pendapatan = @json($total_penjualan);
+                var hari = @json($hari);
+
+                Highcharts.chart('grafik', {
+                    title: {
+                        text: 'Grafik Pendapatan Bulan'
+                    },
+                    xAxis: {
+                        categories: hari
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Nominal Pendapatan Bulanan'
+                        }
+                    },
+                    plotOptions: {
+                        series: {
+                            allowPointSelect: true
+                        }
+                    },
+                    series: [{
+                        name: 'Nominal Pendapatan',
+                        data: pendapatan
+                    }]
+                });
+            </script>
         </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
